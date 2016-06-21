51607103371f225edc38457f474d09a587b0912d
Martín Chamarro <martin.chamarro@gmail.com>
2016-06-21 16:53:15 +0200

    EDIT -Added C code to support the new icons

--- src/icons.h
+++ src/icons.h
@@ -1,11 +1,35 @@
 #pragma once
 #include <pebble.h>
 
-	
+#define ICON_ANDROID "android"
 #define ICON_CART "cart"
+#define ICON_COMMENT "comment"
+#define ICON_DATABASE "database"
 #define ICON_DELIVERY "delivery"
-#define ICON_MONEY "money"
+#define ICON_DISLIKE "dislike"
 #define ICON_DOLLAR "dollar"
-	
+#define ICON_DOWNLOAD "download"
+#define ICON_FACEBOOK "facebook"
+#define ICON_FAV "fav"
+#define ICON_FUNNEL "funnel"
+#define ICON_GOOGLEPLUS "googleplus"
+#define ICON_INSTAGRAM "instagram"
+#define ICON_INSTALL "install"
+#define ICON_IOS "ios"
+#define ICON_LIKE "like"
+#define ICON_LOCATION "location"
+#define ICON_MEMORY "memory"
+#define ICON_MONEY "money"
+#define ICON_PERCENTAGE "percentage"
+#define ICON_PERFORMANCE "performance"
+#define ICON_SERVER "server"
+#define ICON_SHARE "share"
+#define ICON_SHOP "shop"
+#define ICON_STAR "star"
+#define ICON_TIME "time"
+#define ICON_TWITTER "twitter"
+#define ICON_UPLOAD "upload"
+#define ICON_USER "user"
+#define ICON_YOUTUBE "youtube"
 	
 void set_icon_from_id(BitmapLayer* layer, char *id);
